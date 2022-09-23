9230e07da3caeefe14f9286d563da7c38ff663a7
Vineeth Chennuru <chennuruvineeth@gmail.com>
2022-09-23 19:16:14 -0400

    Fixed fig.show() error

--- HW-2/streamlit_plotly.py
+++ HW-2/streamlit_plotly.py
@@ -3,10 +3,8 @@ import numpy as np
 
 import plotly.express as px
 df = px.data.iris()
-sfig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
                     color='petal_length', symbol='species',title='IRIS data represented in 3D scatter plot')
-fig
-fig.show()
 
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
